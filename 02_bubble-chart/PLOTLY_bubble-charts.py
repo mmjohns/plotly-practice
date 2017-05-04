942f8a1310c1b2461a857a216280dd3954b82d95
mmjohns <meganjohs09@gmail.com>
2017-05-04 15:56:01 -0400

    Deleted unused files

---
+++ 02_bubble-chart/PLOTLY_bubble-charts.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# # Plotly: Bubble Charts
+# ## Practicing and experimenting with Plotly, Python and Data Visualization.
+# 
+# #### Links:
+# - With help from [Plotly User Guide](http://nbviewer.jupyter.org/github/plotly/python-user-guide/tree/master/)
+# 
+# - Stored in [GitHub](https://github.com/mmjohns/plotly-practice)
+
+# ----
+
+# ### Contents
+# 1. [**Setup**](#section1)
+# 2. [**Single Line Chart**](#section2)
+# 3. [**Dual Line Chart**](#section3)
+
+# -----
+
+# ## <a id="section1">Set up</a>
+
+# In[ ]:
+
+
+
